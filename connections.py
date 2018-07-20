adc4b2906183b5db064708b3f71d3019997cfb25
francisco-codefresh <39525266+francisco-codefresh@users.noreply.github.com>
2018-07-20 17:10:14 -0600

    Update connections.py

--- connections.py
+++ connections.py
@@ -2,18 +2,22 @@ import httplib
 import time
 
 start = time.time()
-
-conn = httplib.HTTPSConnection("codefresh.io")
+endpoint = "istio.io"
+conn = httplib.HTTPSConnection(endpoint)
+print "Connecting to", endpoint
 counter = 0
-minutes_to_test = 10
-SLEEP = 2
-while counter < minutes_to_test*60/SLEEP:
+minutes_to_test = 16
+sleep = 5
+while counter < minutes_to_test*60/sleep:
     conn.request("GET", "/", headers={"Connection":" keep-alive"})
     r1 = conn.getresponse()
     r1.read()
-    print r1.status, r1.reason
-    print conn.sock
-    time.sleep(SLEEP)
+    print "Time elapsed:", round(((time.time()- start)/60),1), "minutes"
+    counter+=1
+    print r1.status, r1.reason, conn.sock
+    print
+    time.sleep(sleep)
 
 end = time.time()
-print(end - start)
+print ("----")
+print("END_TIME:", round(((end- start)/60),1), "minutes")
