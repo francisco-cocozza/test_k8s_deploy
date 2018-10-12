0c2f4e6458c76afec1c82342abc368b795648a60
francisco-codefresh <39525266+francisco-codefresh@users.noreply.github.com>
2018-10-12 09:49:33 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ kubectl version --short=true
 echo 'SERVER_VERSION=$(kubectl version --short=true | grep -i server | cut -c18-20 | tr -d .) ==>'
 kubectl version --short=true | grep -i server | cut -c18-20 | tr -d .
 SERVER_VERSION=$(kubectl version --short=true | grep -i server | cut -c18-20 | tr -d .)
-if (( "$SERVER_VERSION" <= "16" )); then echo-using-kubectl-v1.6; else echo using-$SERVER_VERSION; fi
+if (( "$SERVER_VERSION" <= "16" )); then echo using-kubectl-v1.6; else echo using-$SERVER_VERSION; fi
